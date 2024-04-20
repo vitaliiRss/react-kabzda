5a6533747a362728fb902a6a748855963bfa6187
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-04-20 18:10:27 +0200

    add: sprint 3 week 4(24-25 useEffect, cleanup)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -4,7 +4,7 @@ export default {
   title: "useEffect stroies"
 }
 
-export const useEffectExample = () => {
+export const UseEffectExample = () => {
   const [fake, setFake] = useState(1)
   const [counter, setCounter] = useState(1)
   console.log("useEffect");
@@ -28,6 +28,78 @@ export const useEffectExample = () => {
     <>
       <button onClick={() => { setFake(fake + 1) }}>fake</button> {fake}
       <button onClick={() => { setCounter(counter + 1) }}>counter</button> {counter}
+    </>
+  )
+}
+
+export const ResetEffectExample = () => {
+  const [counter, setCounter] = useState(1)
+
+  console.log("component rendered " + counter);
+
+  useEffect(() => {
+    console.log("useEffect occured " + counter);
+
+    return () => {
+      console.log("reset effect " + counter);
+
+    }
+  }, [counter])
+
+  const increase = () => {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <>
+      Hello, counter {counter}
+      <button onClick={increase}>counter</button>
+    </>
+  )
+}
+
+export const KeysTrackerExample = () => {
+  const [text, setText] = useState("")
+
+  console.log("component rendered " + text);
+
+  useEffect(() => {
+    const keyPressHandler = (e: KeyboardEvent) => {
+      setText(text + e.key)
+    }
+
+    window.addEventListener("keypress", keyPressHandler)
+
+    return () => {
+      window.removeEventListener("keypress", keyPressHandler)
+    }
+  }, [text])
+
+  return (
+    <>
+      Type text: {text}
+    </>
+  )
+}
+
+export const SetTimeoutExample = () => {
+  const [text, setText] = useState("")
+
+  console.log("component rendered " + text);
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      setText("3 seconds passed")
+    }, 3000)
+
+    return () => {
+      clearTimeout(timeoutID)
+    }
+  }, [text])
+
+  return (
+    <>
+      Type text: {text}
     </>
   )
 }
